e239df5bda3c37270281316aeb8ffc2873fe2734
vigneshwaran-28 <vigneshrm2858@gmail.com>
2023-11-09 12:47:17 +0530

    initial

---
+++ github.js
@@ -0,0 +1,18 @@
+			github
+			
+in order to initialize the git repo -  	git init
+
+
+stage changes for next commit in git repo, If you need to add multiple files or folders specify the path or file names with separated space - 	git add files or file names
+
+
+If you need to see the status of all the staged files, then you can execute the following command-		git status
+
+
+this command is used to stage all the commits you have made -m is to add message to the commit - 		git commit -m "initial commit"
+
+
+
+
+
+
